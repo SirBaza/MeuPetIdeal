e59db43b3561527d1384e3be140ed0457a40d1be
CarolesHaddad <carolhvs0912@gmail.com>
2025-07-28 22:17:47 -0300

    Initialize Spring Boot project structure with essential configurations and files

---
+++ meupetideal/src/main/java/br/controller/view/MainViewController.java
@@ -0,0 +1,18 @@
+package br.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(path = "principal")
+public class MainViewController {
+
+    @GetMapping("/")
+    public String getHomepage() {
+        return "index.html";
+    }
+    
+    
+}
---
+++ meupetideal/src/main/java/br/edu/iff/ccc/meupetideal/MeupetidealApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.meupetideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeupetidealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeupetidealApplication.class, args);
+	}
+
+}
