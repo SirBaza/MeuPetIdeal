cda6d41ec32ebdd49cefebd1035b574e7b7d0a9c
SirBaza <vitorbaza2002@gmail.com~>
2025-08-25 18:19:12 -0300

    Construção da Estrutura base de fragmentos do layout da aplicação

---
+++ meupetideal/meupetideal/src/main/java/br/edu/iff/ccc/meupetideal/controller/view/OngControllerView.java
@@ -0,0 +1,27 @@
+package br.edu.iff.ccc.meupetideal.controller.view;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping(path = "MeuPetIdeal")
+public class OngControllerView
+{
+        @GetMapping("/ong")
+        public String getListaOng(Model model)
+        {
+            return "listaDeOng.html";
+
+        }
+
+        @GetMapping("/informacoesOng")
+        public String getInformacoesOng(Model model)
+        {
+            return "informacoesOng.html";
+
+        }
+
+}
+
---
+++ meupetideal/meupetideal/src/test/java/br/edu/iff/ccc/meupetideal/MeupetidealApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.meupetideal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeupetidealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
